b2a60bffd267429f82f6e314a3f97f0885d6cc9e
Enisuzunn <enisbesiktasli@gmail.com>
2024-10-15 21:52:23 +0300

    Initial Commit

---
+++ XCodeGit/XCodeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCodeGitApp.swift
+//  XCodeGit
+//
+//  Created by Enis Uzun on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct XCodeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XCodeGitUITests/XCodeGitUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  XCodeGitUITestsLaunchTests.swift
+//  XCodeGitUITests
+//
+//  Created by Enis Uzun on 15.10.2024.
+//
+
+import XCTest
+
+final class XCodeGitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
